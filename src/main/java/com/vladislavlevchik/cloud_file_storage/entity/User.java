fa0d3577db8292abf4d70a329e00d65ab3315322
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-23 21:57:42 +0300

    feat: add User entity

---
+++ src/main/java/com/vladislavlevchik/cloud_file_storage/entity/User.java
@@ -0,0 +1,24 @@
+package com.vladislavlevchik.cloud_file_storage.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
