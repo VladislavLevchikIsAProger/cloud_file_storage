41c64d983899babed7f7208185b3a0a9e7961790
Vladislav Levchik Work <vlad.levchik.ares@icloud.com>
2024-07-24 16:20:34 +0300

    refactor: add User security instead of UserDetails

--- src/main/java/com/vladislavlevchik/cloud_file_storage/service/CustomUserDetailsService.java
+++ src/main/java/com/vladislavlevchik/cloud_file_storage/service/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package com.vladislavlevchik.cloud_file_storage.service;
 
-import com.vladislavlevchik.cloud_file_storage.config.UserDetailsImpl;
 import com.vladislavlevchik.cloud_file_storage.entity.User;
 import com.vladislavlevchik.cloud_file_storage.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +8,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -19,9 +18,13 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> user = repository.findByUsername(username);
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username " + username + " not found"));
 
-        return user.map(UserDetailsImpl::new)
-                .orElseThrow(() -> new UsernameNotFoundException(username + " not found"));
+        return new org.springframework.security.core.userdetails.User(
+                username,
+                user.getPassword(),
+                Collections.emptyList()
+        );
     }
 }
