2a8d8ef86b8a80f71709bf5a424b18fa68e18d38
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-24 22:10:02 +0300

    perf: move logic form controller to service

--- src/main/java/com/vladislavlevchik/cloud_file_storage/controller/AuthController.java
+++ src/main/java/com/vladislavlevchik/cloud_file_storage/controller/AuthController.java
@@ -2,19 +2,9 @@ package com.vladislavlevchik.cloud_file_storage.controller;
 
 import com.vladislavlevchik.cloud_file_storage.dto.MessageDto;
 import com.vladislavlevchik.cloud_file_storage.dto.UserRequestDto;
-import com.vladislavlevchik.cloud_file_storage.dto.UserResponseDto;
-import com.vladislavlevchik.cloud_file_storage.entity.User;
-import com.vladislavlevchik.cloud_file_storage.exception.UserAlreadyExistsException;
-import com.vladislavlevchik.cloud_file_storage.service.UserService;
+import com.vladislavlevchik.cloud_file_storage.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,9 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final UserService service;
-    private final ModelMapper mapper;
-    private final AuthenticationManager authenticationManager;
+    private final AuthService service;
 
     //TODO не забыть удалить
     @GetMapping("/hello")
@@ -34,31 +22,18 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserRequestDto userRequestDto) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(userRequestDto.getUsername(), userRequestDto.getPassword())
-            );
+        service.signIn(userRequestDto);
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            return ResponseEntity.ok(MessageDto.builder()
-                    .message("User " + userRequestDto.getUsername() + " logged in successfully!")
-                    .build()
-            );
-        } catch (BadCredentialsException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                    MessageDto.builder()
-                            .message("Invalid username or password")
-                            .build()
-            );
-        }
+        return ResponseEntity.ok(MessageDto.builder()
+                .message("User " + userRequestDto.getUsername() + " logged in successfully!")
+                .build()
+        );
     }
 
+    //TODO мб вернуть message а не объект
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody UserRequestDto userRequestDto) {
-        User user = service.save(userRequestDto);
-
-        return ResponseEntity.ok(mapper.map(user, UserResponseDto.class));
+        return ResponseEntity.ok(service.registerUser(userRequestDto));
     }
 
 }
