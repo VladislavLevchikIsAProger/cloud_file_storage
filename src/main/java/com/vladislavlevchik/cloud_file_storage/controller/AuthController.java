3efa11bbdbed5d2ebb841301006025952342f548
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-25 21:53:43 +0300

    perf: move logic to service

--- src/main/java/com/vladislavlevchik/cloud_file_storage/controller/AuthController.java
+++ src/main/java/com/vladislavlevchik/cloud_file_storage/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.vladislavlevchik.cloud_file_storage.controller;
 
 import com.vladislavlevchik.cloud_file_storage.dto.MessageDto;
-import com.vladislavlevchik.cloud_file_storage.dto.UserRequestDto;
+import com.vladislavlevchik.cloud_file_storage.dto.UserLoginRequestDto;
+import com.vladislavlevchik.cloud_file_storage.dto.UserRegisterRequestDto;
 import com.vladislavlevchik.cloud_file_storage.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,26 +16,21 @@ public class AuthController {
 
     private final AuthService service;
 
-    //TODO не забыть удалить
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "hello";
-    }
-
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserRequestDto userRequestDto) {
-        service.signIn(userRequestDto);
+    public ResponseEntity<?> login(@RequestBody @Validated UserLoginRequestDto userLoginRequestDto) {
+        service.signIn(userLoginRequestDto);
 
         return ResponseEntity.ok(MessageDto.builder()
-                .message("User " + userRequestDto.getUsername() + " logged in successfully!")
+                .message("User " + userLoginRequestDto.getUsername() + " logged in successfully!")
                 .build()
         );
     }
 
     //TODO мб вернуть message а не объект
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody UserRequestDto userRequestDto) {
-        return ResponseEntity.ok(service.registerUser(userRequestDto));
+    public ResponseEntity<?> register(@RequestBody @Validated UserRegisterRequestDto userRegisterRequestDto) {
+
+        return ResponseEntity.ok(service.registerUser(userRegisterRequestDto));
     }
 
 }
