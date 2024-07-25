ef99e9ed3bdcec8bbf44d61f6dde70ef97506a82
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-25 21:53:22 +0300

    feat: add handleValidationExceptions

--- src/main/java/com/vladislavlevchik/cloud_file_storage/GlobalExceptionHandler.java
+++ src/main/java/com/vladislavlevchik/cloud_file_storage/GlobalExceptionHandler.java
@@ -1,31 +1,44 @@
 package com.vladislavlevchik.cloud_file_storage;
 
 import com.vladislavlevchik.cloud_file_storage.dto.MessageDto;
-import com.vladislavlevchik.cloud_file_storage.exception.UserAlreadyExistsException;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<?> handleUserAlreadyExistsException(UserAlreadyExistsException ex){
-        return ResponseEntity.status(HttpStatus.CONFLICT).
-                body(MessageDto.builder()
-                        .message(ex.getMessage())
-                        .build()
-                );
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseBody
+    public MessageDto handleBadCredentialsException() {
+        return MessageDto.builder().message("Invalid username or password").build();
     }
 
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<?> handleBadCredentialsException(){
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                MessageDto.builder()
-                        .message("Invalid username or password")
-                        .build()
-        );
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return errors;
     }
- }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseBody
+    public MessageDto handleDataIntegrityViolationException() {
+        return MessageDto.builder().message("User already exist").build();
+    }
+}
