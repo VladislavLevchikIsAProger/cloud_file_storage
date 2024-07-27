54e614931398caf1b9a5f2004f69c49abea6b113
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-27 21:04:09 +0300

    feat: add NoArgs and AllArgs

--- src/main/java/com/vladislavlevchik/cloud_file_storage/dto/UserRegisterRequestDto.java
+++ src/main/java/com/vladislavlevchik/cloud_file_storage/dto/UserRegisterRequestDto.java
@@ -3,10 +3,14 @@ package com.vladislavlevchik.cloud_file_storage.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UserRegisterRequestDto {
 
