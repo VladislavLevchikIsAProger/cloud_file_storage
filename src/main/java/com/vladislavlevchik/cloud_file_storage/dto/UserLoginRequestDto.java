86d4143feb967c83b2daff120bf1b0bae8566d5b
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-27 21:04:15 +0300

    feat: add NoArgs and AllArgs

--- src/main/java/com/vladislavlevchik/cloud_file_storage/dto/UserLoginRequestDto.java
+++ src/main/java/com/vladislavlevchik/cloud_file_storage/dto/UserLoginRequestDto.java
@@ -1,10 +1,14 @@
 package com.vladislavlevchik.cloud_file_storage.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UserLoginRequestDto {
 
