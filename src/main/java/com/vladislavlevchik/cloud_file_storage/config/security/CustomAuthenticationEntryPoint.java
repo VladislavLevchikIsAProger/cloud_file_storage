210ce5a22be4ec608d17cd937a05c5ce6a95c2f3
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-07-29 19:57:47 +0300

    perf: delete imports

--- src/main/java/com/vladislavlevchik/cloud_file_storage/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/vladislavlevchik/cloud_file_storage/config/security/CustomAuthenticationEntryPoint.java
@@ -1,11 +1,9 @@
 package com.vladislavlevchik.cloud_file_storage.config.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
