82f22000d19a038b5c73a1b3be7828921cb60c54
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-08-03 17:54:16 +0300

    feat: FileResponseDto

---
+++ src/main/java/com/vladislavlevchik/cloud_file_storage/dto/response/FileResponseDto.java
@@ -0,0 +1,17 @@
+package com.vladislavlevchik.cloud_file_storage.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FileResponseDto {
+
+    private String filename;
+    private String filePath;
+
+}
