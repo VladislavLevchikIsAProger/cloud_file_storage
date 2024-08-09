7fb26126bd45f72a3bf84f3314be290a522a0ee5
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-08-10 00:40:51 +0300

    feat: add UserService

---
+++ src/main/java/com/vladislavlevchik/cloud_file_storage/service/UserService.java
@@ -0,0 +1,44 @@
+package com.vladislavlevchik.cloud_file_storage.service;
+
+import com.vladislavlevchik.cloud_file_storage.dto.request.UserRegisterRequestDto;
+import com.vladislavlevchik.cloud_file_storage.entity.CustomFolder;
+import com.vladislavlevchik.cloud_file_storage.entity.User;
+import com.vladislavlevchik.cloud_file_storage.exception.UserNotFoundException;
+import com.vladislavlevchik.cloud_file_storage.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final ModelMapper mapper;
+    private final PasswordEncoder encoder;
+
+    public List<CustomFolder> getListFolders(String username){
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(
+                        () -> new UserNotFoundException("User " + username + " not found")
+                );
+
+        return user.getFolders();
+    }
+
+    public User getUser(String username){
+        return userRepository.findByUsername(username)
+                .orElseThrow(
+                        () -> new UserNotFoundException("User " + username + " not found")
+                );
+    }
+
+    public void register(UserRegisterRequestDto userRegisterRequestDto) {
+        userRegisterRequestDto.setPassword(encoder.encode(userRegisterRequestDto.getPassword()));
+
+        userRepository.save(mapper.map(userRegisterRequestDto, User.class));
+    }
+}
